593115e9061e1daf2a22346a3732ed06befc1dbf
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-01-20 23:25:17 +0000

    more examples

--- odd.js
+++ odd.js
@@ -4,3 +4,16 @@ const isOdd  = function (num) {
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
+
+function howManyHundreds(num)
+{
+  
+  return Math.floor(num / 100);
+}
+
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
