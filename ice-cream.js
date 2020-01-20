83f9c557a67c18f8e378b6601c0c6faa4cbc81fc
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-01-20 23:37:22 +0000

    fixed mistakes

--- ice-cream.js
+++ ice-cream.js
@@ -1,5 +1,3 @@
-
-
 // Do not modify these first two lines
 let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
@@ -7,8 +5,8 @@ console.log(iceCreamFlavours);
 // Your code below here...
 
 //add chocolate to the end
-console.log(iceCreamFlavours.push("root beer"));
-
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
 //print first flavour
 console.log(iceCreamFlavours[0]);
 
