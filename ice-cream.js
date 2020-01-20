3475909dd6c6c50833bd6f77fdc4c2786586b489
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-01-20 23:35:18 +0000

    practise indexing through arrays and manipulating values

---
+++ ice-cream.js
@@ -0,0 +1,19 @@
+
+
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+//add chocolate to the end
+console.log(iceCreamFlavours.push("root beer"));
+
+//print first flavour
+console.log(iceCreamFlavours[0]);
+
+//print last flavour
+console.log(iceCreamFlavours[5]);
+
+//print how many flavours
+console.log(iceCreamFlavours.length);
