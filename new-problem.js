42740d9e81f4f7a3e27165847c7b2c33e4f6e05d
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-01-21 02:00:33 +0000

    adding how to merge and in ascending order

--- new-problem.js
+++ new-problem.js
@@ -21,3 +21,26 @@ console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
 console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
 console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
 console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
+
+//merging in order
+function merge(array1,array2)
+{
+  let newArray = [];
+  
+  for(var row = 0; row < array1.length; row++)
+  {
+    newArray.push(array1[row]);
+  }
+  for (var column = 0; column < array2.length; column++)
+  {
+    newArray.push(array2[column]);
+  }
+  
+  return newArray.sort(function(a, b) {return a - b} );
+}
+
+
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
